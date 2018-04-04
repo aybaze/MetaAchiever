ebb683ad9c6a75d2df7e0ba62e50de8d30e51d1a
hoa16683 <andreas.holzapfel@gmail.com>
2018-04-04 22:25:29 +0200

    /games

--- api/main.py
+++ api/main.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python
 
-print("hello world")
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+app.debug = True
+
+
+
+@app.route("/")
+def test():
+    return jsonify({1: "ein", 2: "schluessel", 3: "fuer", 4: "schloesser"})
+
+
+@app.route("/games")
+def listGames():
+    return jsonify({1: "quake", 2: "DotA"})
+
+app.run()
+
+
