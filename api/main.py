54e00d04d08d566d342d6ba173866a3c28dcffe5
hoa16683 <andreas.holzapfel@gmail.com>
2018-04-04 23:18:56 +0200

    unlocked achievements with steam.txt for key

--- api/main.py
+++ api/main.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
 
 from flask import Flask, jsonify
+from requests import get
+with open('steam.txt', 'r') as myfile:
+    key = myfile.read()
 
 app = Flask(__name__)
+
 app.debug = True
 
+@app.route("/get")
+def get_BA():
+    r = get("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=440&key="+ key + "&steamid=76561197962272442")
+    rjs = r.json()
+    rjs["playerstats"]["gameName"] = "Kekswichsen 3.0"
+    ach_all = rjs["playerstats"]["achievements"]
+    unlocked = 0
+    for x in ach_all:
+        if x["achieved"] == 1:
+            unlocked = unlocked + 1
+    rjs["private"] = {}
+    rjs["private"]["Unlocked Achievements"] = unlocked
+    return jsonify(rjs)
 
 
 @app.route("/")
@@ -13,7 +30,7 @@ def test():
 
 
 @app.route("/games")
-def listGames():
+def list_Games():
     return jsonify({1: "quake", 2: "DotA"})
 
 app.run()
