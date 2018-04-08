33ad531e5913bc7a3c125903ec5d5e12af4c2745
hoa16683 <andreas.holzapfel@gmail.com>
2018-04-08 23:20:06 +0200

    bla

--- api/connect.py
+++ api/connect.py
@@ -4,9 +4,9 @@ from requests import get
 
 def get_achievements_steam_single(user_id, key, app_id):
     #request player details
-    achievement_list_raw = get("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=" + app_id + "&key="+ key + "&steamid=" + user_id)
+    response = get("http://api.steampowered.com/ISteamUserStats/GetPlayerAchievements/v0001/?appid=" + str(app_id) + "&key="+ key + "&steamid=" + str(user_id))
     #build json object
-    achievement_list = achievement_list_raw.jsonify()
+    achievement_list = response.json()
     return achievement_list
 
 def get_player_achievements_ubisoft(user_id, key, app_id):
