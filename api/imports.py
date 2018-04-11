0701f74357715933a3f22e09ca483f41d5ced91b
Christian Banse <oxisto@aybaze.com>
2018-04-11 23:03:32 +0200

    more optimization

--- api/imports.py
+++ api/imports.py
@@ -21,59 +21,78 @@ def steam(graph: Graph, key: str):
                    key + "&steamid=" + str(steam_id) + "&format=json")
     all_games = response.json()
 
+    app_ids = []
     # Put played games into list
     for game_detail in all_games["response"]["games"]:
-        # Obtain all achievements for a game and store into player if playtime is greater than 0
         if game_detail["playtime_forever"] > 0:
-            app_id = game_detail["appid"]
+            # Convert app_id to string here to make our life easier
+            app_ids.append(game_detail["appid"])
 
-            # Check, if game already exists
-            game: Game = Game.select(graph, app_id).first()
+    for app_id in app_ids:
+        # Check, if game already exists
+        game: Game = Game.select(graph, app_id).first()
 
-            if game is not None:
-                print("Game " + str(app_id) + " already found in graph")
-            else:
-                # Fetch from Steam
-                game = steam_schema_for_game(app_id, key, graph)
+        if game is None:
+            print("Game " + str(app_id) +
+                  " not yet found in graph. Fetching from API...")
 
-            # Associate with player if we found valid game data
-            if game != None:
-                player.games.add(game)
+            # Fetch from Steam
+            game = fetch_game(app_id, key)
 
-                # Push the game
-                graph.push(game)
+        # Associate with player if we found valid game data
+        if game != None:
+            player.games.add(game)
+
+            # Push the game
+            graph.push(game)
 
     graph.push(player)
 
 
-def steam_schema_for_game(app_id: int, key: str, graph: Graph) -> Game:
-    print("Game " + str(app_id) + " not yet found in graph. Fetching from API...")
+def fetch_game(app_id: str, key: str) -> Game:
+    store_info = fetch_store_info(app_id)
 
-    # Get game details for specific game
-    response = get("http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=" +
-                   str(key) + "&appid=" + str(app_id))
-    game_details = response.json()
-
-    # Skip game if no achievements or no gameName
-    if "availableGameStats" not in game_details["game"]:
-        return None
-    if "achievements" not in game_details["game"]["availableGameStats"]:
+    if str(app_id) not in store_info or "data" not in store_info[str(app_id)]:
+        print("Could not retrieve store information for " + str(app_id))
         return None
 
     # Create Graph object for game
     game = Game()
     game.id = app_id
-    game.name = game_details["game"]["gameName"]
 
-    # Walk thru all achievements for game and save into game
-    for a in game_details["game"]["availableGameStats"]["achievements"]:
-        achievement = Achievement()
-        achievement.id = a["name"]
-        achievement.name = a["displayName"]
-        achievement.source = "Steam"
-        if "description" in a:
-            achievement.description = a["description"]
-        achievement.achieved_in = game
-        game.achievements.add(achievement)
+    # Take name from store API
+    game.name = store_info[str(app_id)]["data"]["name"]
+
+    # Check, if the game has achievements, otherwise, we're done now
+    if "achievements" in store_info[str(app_id)]["data"] and store_info[str(app_id)]["data"]["achievements"]["total"] > 0:
+        # Get scehema for game
+        schema = get_schema_for_game(app_id, key)
+
+        # Walk thru all achievements for game and save into game
+        for a in schema["game"]["availableGameStats"]["achievements"]:
+            achievement = Achievement()
+            achievement.id = a["name"]
+            achievement.name = a["displayName"]
+            achievement.source = "Steam"
+
+            # Description is optional
+            if "description" in a:
+                achievement.description = a["description"]
+
+            game.achievements.add(achievement)
 
     return game
+
+
+def fetch_store_info(app_id: list) -> dict:
+    query = "http://store.steampowered.com/api/appdetails?appids=" + \
+        str(app_id) + "&filters=basic,achievements"
+    response = get(query)
+
+    return response.json()
+
+
+def get_schema_for_game(app_id: int, key: str) -> dict:
+    response = get("http://api.steampowered.com/ISteamUserStats/GetSchemaForGame/v2/?key=" +
+                   key + "&appid=" + str(app_id))
+    return response.json()
